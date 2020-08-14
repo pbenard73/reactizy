8cd57dac95c4887e936185f76e155f9e8630b099
BENARD Patrick <p.benard73@gmail.com>
2020-08-14 17:57:31 +0200

    fix store return statement

--- src/store.js
+++ src/store.js
@@ -5,8 +5,9 @@ import reduxer from "./reduxer"
 export default (...args) => {
     if (args[args.length - 1] === true) {
         args.pop()
-        createStore(reduxer(...args), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    } else {
-        createStore(reduxer(...args))
+
+        return createStore(reduxer(...args), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     }
+
+    return createStore(reduxer(...args))
 }
