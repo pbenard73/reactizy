fe80677dbe7ddae4edd67e458581913628b981d2
BENARD Patrick <pbenard73@gmail.com>
2020-08-17 16:46:14 +0200

    fix homepage

---
+++ precache-manifest.765c223e5a26d701b59534c0596deabf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d8c02623ef2e7f94162a6c71e653aa",
+    "url": "/reactizy/index.html"
+  },
+  {
+    "revision": "9e448d1328704650df9a",
+    "url": "/reactizy/static/css/2.72ebba26.chunk.css"
+  },
+  {
+    "revision": "5173390145c51cdbe902",
+    "url": "/reactizy/static/css/main.27beedaf.chunk.css"
+  },
+  {
+    "revision": "9e448d1328704650df9a",
+    "url": "/reactizy/static/js/2.a7f87f8d.chunk.js"
+  },
+  {
+    "revision": "e26f71c5c366ec503c9ccad4189d5943",
+    "url": "/reactizy/static/js/2.a7f87f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5173390145c51cdbe902",
+    "url": "/reactizy/static/js/main.e74171be.chunk.js"
+  },
+  {
+    "revision": "03c69ed0f4a7c975e3f8",
+    "url": "/reactizy/static/js/runtime-main.c3647b05.js"
+  }
+]);
