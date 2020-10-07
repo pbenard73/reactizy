15e670961240d1ac393e78bc3cfc8dc80e7d93fa
BENARD Patrick <pbenard73@gmail.com>
2020-10-07 19:21:58 +0200

    v2.0.4

---
+++ precache-manifest.1d0f9152d9a894434e9e96cf3b945a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9490ef31ea7b97ca490238d398043504",
+    "url": "/reactizy/index.html"
+  },
+  {
+    "revision": "4deeed1f3eac9026415d",
+    "url": "/reactizy/static/css/2.72ebba26.chunk.css"
+  },
+  {
+    "revision": "3de674aa21b507c19679",
+    "url": "/reactizy/static/css/main.1effaef2.chunk.css"
+  },
+  {
+    "revision": "4deeed1f3eac9026415d",
+    "url": "/reactizy/static/js/2.9df5e2a7.chunk.js"
+  },
+  {
+    "revision": "e26f71c5c366ec503c9ccad4189d5943",
+    "url": "/reactizy/static/js/2.9df5e2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de674aa21b507c19679",
+    "url": "/reactizy/static/js/main.1c927f11.chunk.js"
+  },
+  {
+    "revision": "03c69ed0f4a7c975e3f8",
+    "url": "/reactizy/static/js/runtime-main.c3647b05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactizy/precache-manifest.7e73975c776135b3f446a73276c60297.js"
+  "/reactizy/precache-manifest.1d0f9152d9a894434e9e96cf3b945a7e.js"
 );
 
 self.addEventListener('message', (event) => {
