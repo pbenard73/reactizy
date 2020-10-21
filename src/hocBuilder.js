9580efbde528a6717dd82217893b865bbad742b6
BENARD Patrick <pbenard73@gmail.com>
2020-10-21 15:56:26 +0200

    replace arrowfx by function

--- src/hocBuilder.js
+++ src/hocBuilder.js
@@ -4,40 +4,41 @@ import { compose } from "redux"
 
 import withReactizy from "./highOrderComponent"
 
-
-export default (pool = {}) => (...args) => {
-    return (Component, ...fusion) => {
-        if (args.length === 0) {
-            return withReactizy(Component, ...fusion)
-        }
-
-        const isHocFirst = pool[args[0][0]] !== undefined
-        const isArray = subject => subject.constructor.name.toLowerCase() === "array"
-        const reduxerArgs = givenArgs =>
-            isArray(givenArgs[0]) === true && isArray(givenArgs[0][0]) === true ? givenArgs[0] : [...givenArgs]
-
-        const checkReduxers = (GivenComponent, givenArgs) => {
-            if (isArray(givenArgs[0]) === true) {
-                GivenComponent.reduxers = givenArgs
-            } else {
-                GivenComponent.reduxers = [[...givenArgs[0]], givenArgs[1] !== undefined ? [...givenArgs[1]] : []]
+export default function (pool = {}) {
+    return function (...args) {
+        return (Component, ...fusion) => {
+            if (args.length === 0) {
+                return withReactizy(Component, ...fusion)
             }
 
-            return withReactizy(GivenComponent, ...fusion)
-        }
+            const isHocFirst = pool[args[0][0]] !== undefined
+            const isArray = subject => subject.constructor.name.toLowerCase() === "array"
+            const reduxerArgs = givenArgs =>
+                isArray(givenArgs[0]) === true && isArray(givenArgs[0][0]) === true ? givenArgs[0] : [...givenArgs]
 
-        if (isHocFirst === true && args.length === 1) {
-            const uses = map(args, item => (pool[item] === undefined ? null : pool[item]))
+            const checkReduxers = (GivenComponent, givenArgs) => {
+                if (isArray(givenArgs[0]) === true) {
+                    GivenComponent.reduxers = givenArgs
+                } else {
+                    GivenComponent.reduxers = [[...givenArgs[0]], givenArgs[1] !== undefined ? [...givenArgs[1]] : []]
+                }
 
-            return compose(...uses)(withReactizy(Component, ...fusion))
-        } else if (isHocFirst === true && args.length > 1) {
-            let [use, ...rest] = args
+                return withReactizy(GivenComponent, ...fusion)
+            }
 
-            const uses = map(use, item => (pool[item] === undefined ? null : pool[item]))
+            if (isHocFirst === true && args.length === 1) {
+                const uses = map(args, item => (pool[item] === undefined ? null : pool[item]))
 
-            return compose(...uses)(checkReduxers(Component, reduxerArgs(rest)))
-        } else {
-            return checkReduxers(Component, reduxerArgs(args))
+                return compose(...uses)(withReactizy(Component, ...fusion))
+            } else if (isHocFirst === true && args.length > 1) {
+                let [use, ...rest] = args
+
+                const uses = map(use, item => (pool[item] === undefined ? null : pool[item]))
+
+                return compose(...uses)(checkReduxers(Component, reduxerArgs(rest)))
+            } else {
+                return checkReduxers(Component, reduxerArgs(args))
+            }
         }
     }
 }
